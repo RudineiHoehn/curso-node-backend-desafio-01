9f5c0bb2294a40a761b551c346f8e210d6423eeb
Rudinei Luiz Hoehn <rudineilh@gmail.com>
2019-04-30 21:40:14 -0300

    Correção do exercicio

--- index.js
+++ index.js
@@ -12,28 +12,37 @@ nunjucks.configure('view', {
 app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'njk')
 
+const checkAgeQueryParam = (req, res, next) => {
+  const { age } = req.query
+
+  if (!age) {
+    return res.redirect('/')
+  }
+
+  return next()
+}
+
 app.get('/', (req, res) => {
   return res.render('main')
 })
 
 app.post('/check', (req, res) => {
-  const age = req.body.age
+  const { age } = req.body
   if (age < 18) {
     res.redirect(`minor/?age=${age}`)
-  } else if (age >= 18) {
-    res.redirect(`major/?age=${age}`)
   } else {
-    res.redirect('/')
+    res.redirect(`major/?age=${age}`)
   }
 })
 
-app.get('/major', (req, res) => {
-  const age = req.query.age
+app.get('/major', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
+
   res.render('major', { age })
 })
 
-app.get('/minor', (req, res) => {
-  const age = req.query.age
+app.get('/minor', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
   res.render('minor', { age })
 })
 
